c4c05ff8c391b3bec5872727641df95aade8a135
Angelynna Klara Mendes da Silva <s.klara@academico.ifrn.edu.br>
2023-07-05 08:49:09 -0300

    ola

---
+++ jogodanave2023/jogodanave2023 - Copia/Assets/Scripts/Inimigos.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inimigos : MonoBehaviour
+{
+    public float velocidadeDoInimigo;
+    public int vidaMaximaDoInimigo;
+    public int vidaAtualDoInimigo;
+    public int pontosParaDar;
+    public int chanceParaDropar;
+    public GameObject laserDoInimigo;
+    public Transform localDoDisparo;
+    public GameObject itemParaDropar;
+    public float velocidadeDoInimigos;
+    public float tempoMaximoEntreOsLasers;
+    public float tempoAtualDosLasers;
+    public bool inimigoAtirador;
+    public bool inimigoAtivado;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        inimigoAtivado = false;
+
+        vidaAtualDoInimigo = vidaMaximaDoInimigo;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoviventarInimigo();
+        
+        if (inimigoAtirador== true && inimigoAtivado == true)
+        {
+           
+            AtirarLaser();
+        }
+    }
+
+    public void AtivarInimigo()
+    {
+     inimigoAtivado = true;
+    }
+
+          private void MoviventarInimigo()
+    {
+      transform.Translate(Vector3.down * velocidadeDoInimigo *Time.deltaTime);
+    }
+    
+          private void AtirarLaser()
+    {
+        tempoAtualDosLasers -= Time.deltaTime;
+        if (tempoAtualDosLasers <=0)
+        {
+            Instantiate(laserDoInimigo, localDoDisparo.position, Quaternion.Euler(0f, 0f, 90f));
+            tempoAtualDosLasers = tempoMaximoEntreOsLasers;
+        }
+    }
+    //quando a vida do inimigo for igual a 0 ele ira se destruir.
+    public void MachucarInimigo(int danoPraraReceber)
+    {
+        vidaAtualDoInimigo -=danoPraraReceber;
+        if(vidaAtualDoInimigo <= 0)
+        {
+            GameManager.instance.AumentarPontuacao(pontosParaDar);
+
+          int numeroAleatorio=Random.Range(0,100);
+          if(numeroAleatorio<= chanceParaDropar)
+          {
+            Instantiate(itemParaDropar,transform.position,Quaternion.Euler(0f,0f,0f));
+          }
+
+            Destroy(this.gameObject);
+        }
+    }
+}
